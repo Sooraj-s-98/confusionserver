e539b92bb83688405e742fb2fa3b09c9b418f6cd
sooraj_s <sooraj1998ayiroor@gmail.com>
2020-07-27 17:58:23 +0530

    File Upload

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 var passport = require('passport');
 var config = require('./config');
+const uploadRouter = require("./routes/uploadRouter");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
@@ -64,6 +65,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/dishes", dishRouter);
 app.use("/promotions", promoRouter);
 app.use("/leaders", leaderRouter);
+app.use('/imageUpload',uploadRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
