8c208ebd96888982745d181f3c3c098f16123409
sooraj_s <sooraj1998ayiroor@gmail.com>
2020-07-28 12:54:07 +0530

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    facebook: {
+        clientId: '818426178694814',
+        clientSecret: 'c7cf723d16392c682fe05d5b88d9bfee'
+      }
 }
