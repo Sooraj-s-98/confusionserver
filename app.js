8dc2c290b7dc9068510f1411a590444e28814af4
sooraj_s <sooraj1998ayiroor@gmail.com>
2020-07-28 15:33:02 +0530

    Assignment 4

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var logger = require("morgan");
 var passport = require('passport');
 var config = require('./config');
 const uploadRouter = require("./routes/uploadRouter");
+var favoriteRouter = require("./routes/favoriteRouter");
+
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
@@ -66,6 +68,7 @@ app.use("/dishes", dishRouter);
 app.use("/promotions", promoRouter);
 app.use("/leaders", leaderRouter);
 app.use('/imageUpload',uploadRouter);
+app.use("/favorites", favoriteRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
