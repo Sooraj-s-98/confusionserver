6fc780990d6196defc70142d9690b670b10f06eb
Sooraj s <52352285+Sooraj-s-98@users.noreply.github.com>
2020-08-01 11:54:28 +0530

    hide facebook clientSecret

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
     'secretKey': '12345-67890-09876-54321',
     'mongoUrl' : 'mongodb://localhost:27017/conFusion',
     facebook: {
-        clientId: '818426178694814',
-        clientSecret: 'c7cf723d16392c682fe05d5b88d9bfee'
+        clientId: '***********************',
+        clientSecret: '********************'
       }
 }
